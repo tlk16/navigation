6379228957cb183874620ea6f656139c3dd10bd6
tlk <tlk@mails.tsinghua.edu.cn>
2019-12-26 00:17:24 +0800

    refine codes

--- experiment_tie/exp_bsd.py
+++ experiment_tie/exp_bsd.py
@@ -17,7 +17,7 @@ class Rat():
     agent reset, (act, remember), train
     """
 
-    def __init__(self, memory_size=50, input_type='touch', train_paras='two', device='cuda:1'):
+    def __init__(self, memory_size=1000, input_type='touch', train_paras='two', device='cuda:1'):
         if input_type == 'touch':
             self.net = RNN(input_size=4, action_size=2, hidden_size=512, output_size=8).to(device)
         elif input_type == 'pos':
@@ -41,24 +41,25 @@ class Rat():
         self.lam = 0.3
         self.alpha = 0.2
 
+        self.batch_size = int(self.memory_size/10)
+
         # changed
         self.epsilon = 0.5
 
-        self.num_step = 0
         self.hidden_state = self.net.initHidden().to(device)
         self.last_action = self._initAction()
         self.sequence = None
         self.phase = 'train'
         self.losses = []
 
-    def _epsilon_choose_action(self, q_value, epsilon=0.2):
+    def _epsilon_choose_action(self, q_value):
         """
         epsilon-greedy choose action according to q_value
         :param q_value: tensor, size=[1,8]
         :param epsilon: the degree of greed, float 0~1
         :return: acion, int
         """
-        if random.random() > epsilon:
+        if random.random() > self.epsilon:
             return self._greedy_choose_action(q_value)
 
         return random.randint(0, self.action_space - 1)
@@ -93,36 +94,41 @@ class Rat():
         else:
             raise TypeError('input tpye wrong')
 
-
         with torch.no_grad():
             touch = torch.from_numpy(input).float().to(self.device)  # touch.shape = [4,]
             output, self.hidden_state = self.net(touch, self.hidden_state,
                                                  torch.from_numpy(self.int2angle(self.last_action)).float().to(self.device))
             # output.shape = [1,8] self.hidden_state.shape = [1,512]
-        self.num_step += 1
-        if self.phase == 'train':
-            return self._epsilon_choose_action(output, epsilon=self.epsilon)
-        return self._greedy_choose_action(output)
 
-    def reset(self, init_net=True, phase='train'):
+        if self.phase == 'train':
+            action = self._epsilon_choose_action(output)
+        elif self.phase == 'test':
+            action = self._greedy_choose_action(output)
+        else:
+            raise TypeError('rat.phase wrong')
+
+        self.last_action = action
+        return action
+
+    def reset(self, init_net, init_sequence, phase):
         """
-        reset hidden_state, phase, nump_step and sequence
+        reset hidden_state, phase, last_action and sequence
         """
 
         if init_net:
             self.hidden_state = self.net.initHidden().to(self.device)
 
         self.last_action = self._initAction()
-        self.num_step = 0
         self.phase = phase
-        self.sequence = {
-            'hidden0': self.hidden_state,
-            'positions': [],
-            'observations': [],
-            'touches': [],
-            'rewards': [],
-            'actions': []
-        }
+        if init_sequence:
+            self.sequence = {
+                'hidden0': self.hidden_state,
+                'positions': [],
+                'observations': [],
+                'touches': [],
+                'rewards': [],
+                'actions': []
+            }
 
     def remember(self, state, reward, action, done):
         # a_(t-1), s_t
@@ -174,34 +180,38 @@ class Rat():
         else:
             raise TypeError('train paras wrong')
 
+        if self.batch_size <= len(self.memory):
+            train_memory = random.sample(self.memory, self.batch_size)
+        else:
+            train_memory = self.memory
         loss_all = []
         Optimizer_q.zero_grad()
         if self.input_type == 'touch':
-            inputs = torch.stack([sequence['touches'] for sequence in self.memory])
+            inputs = torch.stack([sequence['touches'] for sequence in train_memory])
         elif self.input_type == 'pos':
-            inputs = torch.stack([sequence['positions'] for sequence in self.memory])
+            inputs = torch.stack([sequence['positions'] for sequence in train_memory])
         else:
             raise TypeError('input type wrong')
 
         q_predicts = self.net.forward_sequence_values(
             inputs.float(),
-            torch.stack([sequence['hidden0'] for sequence in self.memory]).float(),
-            torch.stack([sequence['action_angles'] for sequence in self.memory]).float()
+            torch.stack([sequence['hidden0'] for sequence in train_memory]).float(),
+            torch.stack([sequence['action_angles'] for sequence in train_memory]).float()
         )
-        actions = torch.stack([sequence['actions'] for sequence in self.memory]).float()
-        # print(actions)
+
+        actions = torch.stack([sequence['actions'] for sequence in train_memory])
+        actions = actions.unsqueeze(2).long()
+
+        rewards = torch.stack([sequence['rewards'] for sequence in train_memory])
+        rewards = rewards.unsqueeze(2)
+
         q_predicts = torch.stack(q_predicts).permute((1, 0, 2))
-        Qs = self.value_back(q_predicts, actions.unsqueeze(2).long(),
-                             torch.stack([sequence['rewards'] for sequence in self.memory]).unsqueeze(2)
-                             )
-        # print(q_predicts.shape, Qs.shape)
-        if len(Qs) >= 2:
-            loss_q = torch.mean((q_predicts[:, :-1, :] - Qs.detach()) ** 2)
-            loss_q.backward()
-            # print('loss', loss_q * 1000)
-            loss_all.append(10 * loss_q.detach().item())
-        # for para in self.net.parameters():
-        #     print(para.grad)
+
+        Qs = self.value_back(q_predicts, actions, rewards)
+
+        loss_q = torch.mean((q_predicts[:, :-1, :] - Qs.detach()) ** 2)
+        loss_q.backward()
+        loss_all.append(loss_q.detach().item())
         Optimizer_q.step()
         self.losses.append(np.array(loss_all).mean())
 
@@ -240,42 +250,31 @@ class Session:
         self.rewards = {'train':[], 'test':[]}
         self.mean_rewards = {'train':[], 'test':[]}
 
-    def reset_body(self):
-        self.env.reset()             # pos or random
-        self.rat.reset()
-
     def episode(self, epochs=10):
 
         for epoch in range(epochs):
-            self.rat.reset(self.phase)
             sum_step = 0
             sr = 0
             while sum_step < self.env.limit:
+                self.rat.reset(init_net=(sum_step==0), init_sequence=(sum_step==0), phase=self.phase)
                 state, reward, done, _ = self.env.reset()
                 sr += reward
                 sum_step += 1
                 if self.phase == 'train':
-                    self.rat.remember(state, reward, self.rat.action0, sum_step == self.env.limit)
-                else:
-                    self.rat.remember(state, reward, self.rat.action0, done=False)
+                    self.rat.remember(state, reward, self.rat.last_action, sum_step == self.env.limit)
                 if sum_step == self.env.limit:
                     break
-
 
                 while not done:
                     action = self.rat.act(state)
                     state, reward, done, _ = self.env.step(self.rat.int2angle(action))
-                    # print(sum_step)
+                    sr += reward
                     sum_step += 1
 
                     if self.phase == 'train':
                         self.rat.remember(state, reward, action, sum_step == self.env.limit)
                     if sum_step == self.env.limit:
                         break
-
-                    # print(self.rat.num_step, self.rat.int2angle(action), state[0], state[2], step)
-                # print(self.phase, epoch, 'running reward', reward)
-                    sr += reward
 
             self.rewards[self.phase].append(sr)
 
@@ -290,7 +289,7 @@ class Session:
         elif self.phase == 'test':
             self.episode(epochs)
         else:
-            print('wrong phase')
+            raise TypeError('session.phase wrong')
 
     def save_png(self, filename):
         def smooth(list_a, n=3):
@@ -344,7 +343,7 @@ if __name__ == '__main__':
                 if epsilon[0] - i * epsilon[1] > epsilon[2] else epsilon[2]
 
             session.phase = 'train'
-            session.experiment(epochs=50)
+            session.experiment(epochs=5)
 
             session.phase = 'test'
             session.experiment(epochs=10)
